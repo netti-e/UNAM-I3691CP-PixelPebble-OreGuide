f11cf957d3426c8cdb4be53597591e1773daa934
timoteus <ekandjojason33@gmail.com>
2026-06-08 23:48:37 +0200

    fixed remaining errors and added packages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -117,6 +117,7 @@ function RootLayoutContent() {
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', headerShown: false }} />
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
